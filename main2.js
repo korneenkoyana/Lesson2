526530a8f99f9d43add82489846dc68d4fd63228
korneenkoyana <96310576+korneenkoyana@users.noreply.github.com>
2022-03-25 10:16:38 +0300

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,22 @@
+
+/* 
+    Задание 2:
+
+    Необходимо создать объект в котором вы опишите самого себя в формате : "свойство: значение"
+
+    Список свойств:
+
+    - Имя(string)
+    - Фамилия(string)
+    - Возраст(number)
+    - Есть ли домашние животные(boolean)
+*/
+
+const infoYana = {
+    name : 'Yana',
+    secondName : 'Karneyenka',
+    year : 23,
+    pet : true
+};
+
+console.log(infoYana);
---
+++ main4.js
@@ -0,0 +1,27 @@
+
+/* 
+    Задание 4:
+
+    Напишите функцию, в теле которой будет находится следующий код:
+
+    const fullName = `${firstName} ${lastName}`;
+    console.log(fullName);
+
+    firstName и lastName - это параметры функции
+
+    При вызове функции передайте ей, в качестве аргумента, свое имя и фамилию.
+*/
+
+
+
+function fullName(firstName, lastName){
+    const fullName = `${firstName} ${lastName}`;
+    console.log(fullName);
+    
+}
+fullName('Yana', 'Karneyenka')
+
+
+
+
+
